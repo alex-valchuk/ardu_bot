10c79262300a0df562db65b877df3936c0a6ee29
Alex Valchuk <alex.valchuk@deltatre.it>
2022-10-01 20:05:23 +0300

    slow down the wheels

--- ardu_bot.ino
+++ ardu_bot.ino
@@ -4,6 +4,13 @@
 
 const int Max_dist = 300;
 
+// The speed of the motor depends on the value that was passed to the analogWrite function.
+// Remember the value of 'Motors_speed' can be between 0 and 255. 
+// If you pass 0, then the motors will stop and if you pass 255 then they will run at full speed.
+const int Left_motors_speed_pin = 3;
+const int Right_motors_speed_pin = 6;
+const int Motors_speed = 127;
+
 const int left_ang = 160;
 const int between_left_and_front_ang = 125;
 const int front_ang = 90;
@@ -28,8 +35,11 @@ void setup()
 
   neck.attach(14);
 
-  setup_motor_system(2, 3, 4, 5);
- 
+  setup_motor_system(2, 4, 7, 8);
+  
+  pinMode(Left_motors_speed_pin, OUTPUT);
+  pinMode(Right_motors_speed_pin, OUTPUT);
+
   _stop();
 }
 
@@ -70,6 +80,8 @@ bool potentially_stuck(int Dist_front) {
 void loop()
 {
   _stop();
+  analogWrite(Left_motors_speed_pin, Motors_speed);
+  analogWrite(Right_motors_speed_pin, Motors_speed);
     
   int dist_to_move = 0;  
 
@@ -84,41 +96,41 @@ void loop()
   if (stuck_counter > 1) {
     turn_around();
     stuck_counter = 0;
+    return;
+  }
+
+  if (Dist_front > 30) {
+    dist_to_move = Dist_front;
   }
   else {
-    if (Dist_front > 30) {
-      dist_to_move = Dist_front;
-    }
-    else {
-      int Dist_left = measure_dist(left_ang);
-      int Dist_right = measure_dist(right_ang);
-    
-      rotate_neck(front_ang); // to always look forward
-    
-      if ((Dist_left > Dist_front) && (Dist_left > Dist_right))
-      {
-        left();
-        delay(time_90);
-        dist_to_move = Dist_left;
-      }
-      else if (Dist_right > Dist_front)
-      {
-        right();
-        delay(time_90);
-        dist_to_move = Dist_right;
-      }
-      else
-      {
-        right();
-        delay(time_180);
-      }
-    }
+    int Dist_left = measure_dist(left_ang);
+    int Dist_right = measure_dist(right_ang);
   
-    double dist = dist_to_move * 0.85;
-    int time_for_move = time_for_start_moving + dist * time_for_1cm;
-    forward();
-    delay(time_for_move);
+    rotate_neck(front_ang); // to always look forward
   
-    dist_front_prev = Dist_front;
+    if ((Dist_left > Dist_front) && (Dist_left > Dist_right))
+    {
+      left();
+      delay(time_90);
+      dist_to_move = Dist_left;
+    }
+    else if (Dist_right > Dist_front)
+    {
+      right();
+      delay(time_90);
+      dist_to_move = Dist_right;
+    }
+    else
+    {
+      right();
+      delay(time_180);
+    }
   }
+  
+  double dist = dist_to_move * 0.85;
+  int time_for_move = time_for_start_moving + dist * time_for_1cm;
+  forward();
+  delay(time_for_move);
+  
+  dist_front_prev = Dist_front;
 }
