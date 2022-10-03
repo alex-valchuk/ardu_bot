2a8cfe46bba7c1e4f397c9b5e799692d3215f487
Alex Valchuk <alex.valchuk@deltatre.it>
2022-10-03 21:01:16 +0300

    Start global vars with capitals

--- ardu_bot.ino
+++ ardu_bot.ino
@@ -9,22 +9,22 @@ const int Max_dist = 300;
 // If you pass 0, then the motors will stop and if you pass 255 then they will run at full speed.
 const int Left_motors_speed_pin = 3;
 const int Right_motors_speed_pin = 6;
-const int Motors_speed = 127;
+const int Motors_speed = 180;
 
-const int left_ang = 160;
-const int between_left_and_front_ang = 125;
-const int front_ang = 90;
+const int Left_ang = 160;
+const int Between_left_and_front_ang = 125;
+const int Front_ang = 90;
 const int between_right_and_front_ang = 55;
-const int right_ang = 20;
+const int Right_ang = 20;
 
-const int time_90 = 390;
-const int time_180 = 750;
-const int time_for_start_moving = 100;
-const int time_for_1cm = 10;
-const int time_for_neck_step_rotation = 100;
+const int Time_90 = 390;
+const int Time_180 = 750;
+const int Time_for_start_moving = 100;
+const int Time_for_1cm = 10;
+const int Time_for_neck_step_rotation = 100;
 
-int stuck_counter = 0;
-int dist_front_prev = 0;
+int Stuck_counter = 0;
+int Dist_front_prev = 0;
 
 Servo neck;
 int current_neck_angle = 90;
@@ -51,7 +51,7 @@ void rotate_neck(int angle) {
   while (current_neck_angle != angle) {
     current_neck_angle += rotation_step;
     neck.write(current_neck_angle);
-    delay(time_for_neck_step_rotation);
+    delay(Time_for_neck_step_rotation);
   }
 }
 
@@ -62,9 +62,9 @@ int measure_dist(int angle) {
 
 void turn_around() {
   backward();
-  delay(time_90);
+  delay(Time_90);
   right();
-  delay(time_90);
+  delay(Time_90);
 }
 
 bool between(int a, int b, int accuracy) {
@@ -73,8 +73,8 @@ bool between(int a, int b, int accuracy) {
 
 bool potentially_stuck(int Dist_front) {
   return 
-    Dist_front == dist_front_prev
-    || between(Dist_front, dist_front_prev, 5);
+    Dist_front == Dist_front_prev
+    || between(Dist_front, Dist_front_prev, 5);
 }
 
 void loop()
@@ -85,17 +85,17 @@ void loop()
     
   int dist_to_move = 0;  
 
-  int Dist_front = measure_dist(front_ang);
+  int Dist_front = measure_dist(Front_ang);
   if (potentially_stuck(Dist_front)) {
-    stuck_counter++;
+    Stuck_counter++;
   }
   else {
-    stuck_counter = 0;
+    Stuck_counter = 0;
   }
 
-  if (stuck_counter > 1) {
+  if (Stuck_counter > 1) {
     turn_around();
-    stuck_counter = 0;
+    Stuck_counter = 0;
     return;
   }
 
@@ -103,34 +103,34 @@ void loop()
     dist_to_move = Dist_front;
   }
   else {
-    int Dist_left = measure_dist(left_ang);
-    int Dist_right = measure_dist(right_ang);
+    int Dist_left = measure_dist(Left_ang);
+    int Dist_right = measure_dist(Right_ang);
   
-    rotate_neck(front_ang); // to always look forward
+    rotate_neck(Front_ang); // to always look forward
   
     if ((Dist_left > Dist_front) && (Dist_left > Dist_right))
     {
       left();
-      delay(time_90);
+      delay(Time_90);
       dist_to_move = Dist_left;
     }
     else if (Dist_right > Dist_front)
     {
       right();
-      delay(time_90);
+      delay(Time_90);
       dist_to_move = Dist_right;
     }
     else
     {
       right();
-      delay(time_180);
+      delay(Time_180);
     }
   }
   
   double dist = dist_to_move * 0.85;
-  int time_for_move = time_for_start_moving + dist * time_for_1cm;
+  int time_for_move = Time_for_start_moving + dist * Time_for_1cm;
   forward();
   delay(time_for_move);
   
-  dist_front_prev = Dist_front;
+  Dist_front_prev = Dist_front;
 }
